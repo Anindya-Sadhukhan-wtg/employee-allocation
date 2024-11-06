0bdcfcde495362287e5d40986f5bde81fdc5cc8e
Anindya.Sadhukhan <anindya.sadhukhan@wisetechglobal.com>
2024-11-06 10:41:32 +0530

    Initial commit: created project

---
+++ src/main/java/org/example/employeeallocation/EmployeeAllocationApplication.java
@@ -0,0 +1,13 @@
+package org.example.employeeallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAllocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/employeeallocation/EmployeeAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.employeeallocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
